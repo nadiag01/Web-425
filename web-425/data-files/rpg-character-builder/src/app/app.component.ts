788613c7d022bdbd330730c0cb0b46af4db18816
Nadia <nadiagainer@gmail.com>
2024-08-24 03:27:03 -0500

    update

--- web-425/data-files/rpg-character-builder/src/app/app.component.ts
+++ web-425/data-files/rpg-character-builder/src/app/app.component.ts
@@ -6,10 +6,39 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  template: `
+    <nav>
+      <a routerLink="/players">Players</a>
+      <a routerLink="/signin">Sign In</a>
+      <a routerLink="/create-character">Create Character</a>
+      <a routerLink="/create-guild">Create Guild</a>
+      <a routerLink="/character-faction">Character Faction</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    nav a {
+      margin: 10px;
+      text-decoration: none;
+      color: blue;
+    }
+    nav a:hover {
+      text-decoration: underline;
+    }
+  `]
 })
+
+
+
 export class AppComponent {
   title = 'rpg-character-builder';
 }
+
+
+
+
+
+
 
 
--- web-425/data-files/rpg-character-builder/src/app/app.routes.ts
+++ web-425/data-files/rpg-character-builder/src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Import HomeComponent
+import { PlayersComponent } from './players/players.component';
+import { SigninComponent } from './signin/signin.component';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { CreateGuildComponent } from './create-guild/create-guild.component';
+import { CharacterFactionComponent } from './character-faction/character-faction.component';
+
+
+export const appRoutes: Routes = [
+  { path: 'players', component: PlayersComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'create-character', component: CreateCharacterComponent },
+  { path: 'create-guild', component: CreateGuildComponent },
+  { path: 'character-faction', component: CharacterFactionComponent },
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+];
+
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Default route
@@ -12,3 +29,5 @@ export const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
