d5a1857e1b3090a8d2650a8e14e3cb645a490dae
Nadia <nadiagainer@gmail.com>
2024-09-29 14:00:31 -0500

    create-character update

--- web-425/data-files/rpg-character-builder/src/app/create-character/create-character.component.ts
+++ web-425/data-files/rpg-character-builder/src/app/create-character/create-character.component.ts
@@ -1,38 +1,90 @@
 import { Component } from '@angular/core';
+import {  FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 interface Character {
-  id: number;
   name: string;
   gender: string;
   class: string;
+  faction?: string;
+  startingLocation?: string;
+  funFact?: string;
+}
+
+interface NewCharacter {
+  character: Character;
+  characterId: number;
 }
 
 @Component({
   selector: 'app-create-character',
-  template: ` <p>create-character works!</p> `,
-  styles: ``,
-  standalone:true
+  templateUrl: './create-character.component.html',
+  styleUrl: `./create-character.component.css`,
+  standalone:true,
+  imports: [FormsModule, CommonModule]
 })
 export class CreateCharacterComponent {
-  characters: Character[] = [];
-  character: Character = { id: 0, name: '', gender: '', class: '' };
+  characters: Character[] = [
+    { name: 'Thorn', gender: 'Male', class: 'Warrior', faction: 'The Iron Brotherhood', startingLocation: 'Ironhold', funFact: 'Thorn once single-handedly defeated a dragon.' },
+    { name: 'Badger', gender: 'Female', class: 'Mage', faction: 'The Forest', startingLocation: 'Cavern', funFact: 'Badger has protective skin that is tough to penetrate in combat.' },
+    { name: 'Tank', gender: 'Male', class: 'Rogue', faction: 'Hounds of the Oasis', startingLocation: 'Hideaway', funFact: 'Tank can craft and store up to 5 weapons' },
+    { name: 'Rose', gender: 'Female', class: 'Warrior', faction: 'The Iron Brothers', startingLocation: 'Ironhold', funFact: 'Rose has the ability to take down seven enemies in one strike.' },
+    { name: 'L', gender: 'Male', class: 'Mage', faction: 'The Forest', startingLocation: 'Cavern', funFact: 'L is super intellegent and witty, he uses this to his advantage.' },
+    { name: 'Suki', gender: 'Female', class: 'Rogue', faction: 'Hounds of the Oasis', startingLocation: 'Hideaway', funFact: 'Suki has a great eye of buying favorable goods.' },
+    { name: 'Eren', gender: 'Male', class: 'Warrior', faction: 'The Iron Brotherhood', startingLocation: 'Ironhold', funFact: 'Eren can defeat enemies twice his size with ease' },
+    { name: 'Cloud', gender: 'Female', class: 'Mage', faction: 'The Forest', startingLocation: 'Cavern', funFact: 'Cloud carries a huge sword.' },
+    { name: 'Minato', gender: 'Male', class: 'Rouge', faction: 'Hounds of the Oasis', startingLocation: 'Hideaway', funFact: 'Minato has lightning speed.' },
+    { name: 'Usagi', gender: 'Female', class: 'Warrior', faction: 'The Ironbrotherhood', startingLocation: 'Ironhold', funFact: 'Fights for love and justice.' },
+  ];
+  character: Character = { name: '', gender: '', class: '' };
+  characterId: number;
+  name: string;
+  gender: string;
+  class: string;
+  newCharacter: NewCharacter;
+
+  constructor() {
+    this.characterId = 0;
+    this.name = "";
+    this.gender = "";
+    this.class = "";
+    this.newCharacter = {character: this.character, characterId: 0}
+  }
 
   // Handle form submission
-  onSubmit(form: any) {
-    if (form.valid) {
-      const newCharacter: Character = {
-        ...this.character,
-        id: Math.floor(Math.random() * 1000) + 1, // Random ID between 1 and 1000
-      };
+  onSubmit() {
+    this.characterId = Math.floor(Math.random() * 1000) + 1;
 
-      this.characters.push(newCharacter);
-      this.resetForm(form); // Reset form after submission
+    let formCharacter = {
+      name: this.name,
+      gender: this.gender,
+      class: this.class
     }
+
+    this.characters.push(formCharacter);
+
+    this.newCharacter = {
+      character: formCharacter,
+      characterId: this.characterId
+    }
+
+    this.resetForm()
+    // if (form.valid) {
+    //   const newCharacter: Character = {
+    //     ...this.character,
+    //     id: Math.floor(Math.random() * 1000) + 1, // Random ID between 1 and 1000
+    //   };
+
+    //   this.characters.push(newCharacter);
+    //   this.resetForm(form); // Reset form after submission
+    // }
   }
 
   // Reset the form fields
-  resetForm(form: any) {
-    form.resetForm();
-    this.character = { id: 0, name: '', gender: '', class: '' };
+  resetForm() {
+    //form.resetForm();
+    this.name = "";
+    this.gender = "";
+    this.class = ""
   }
 }
