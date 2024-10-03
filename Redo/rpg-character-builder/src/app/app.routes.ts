82cb8f9040afeb841a79cce0e3b57c7d97802ea8
Nadia <nadiagainer@gmail.com>
2024-10-03 16:24:06 -0500

    project start

--- Redo/rpg-character-builder/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component';
-
-export const routes: Routes = [
-  {path:'',component:AppComponent},
-  {path: 'home', component: HomeComponent}
-];
